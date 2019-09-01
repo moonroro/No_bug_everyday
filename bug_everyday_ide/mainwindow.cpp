92520a26a3418242b67d734eedf72adb5a499578
jdx926 <52766911+jdx926@users.noreply.github.com>
2019-09-01 15:26:59 +0800

    jdx0901

--- bug_everyday_ide/mainwindow.cpp
+++ bug_everyday_ide/mainwindow.cpp
@@ -1,17 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->resize(1000,800);
-    ui->splitter->setStretchFactor(0,1);
-    ui->splitter->setStretchFactor(1,7);                  //设置splitter的比例为1:7 ,0表示右（上），1表示左（下）
+    ui->splitter->setStretchFactor(0,1);             //设置splitter的两个框的比例为 ,0表示右（上），1表示左（下） 右边表示1:7
+    ui->splitter->setStretchFactor(1,7);
     ui->splitter_2->setStretchFactor(0,6);
     ui->splitter_2->setStretchFactor(1,2);
+
+    /*以下部分是函数信号槽*/
+    connect(ui->actioncpy,SIGNAL(triggered()),this,SLOT(on_copy()));
+    connect(ui->actioncut,SIGNAL(triggered()),this,SLOT(cut_it()));
+    connect(ui->actionpst,SIGNAL(triggered()),this,SLOT(paste_it()));
+    connect(ui->actionsal,SIGNAL(triggered()),this,SLOT(select_all()));
+    connect(ui->action_redo,SIGNAL(triggered()),this,SLOT(redo_it()));
+    connect(ui->actionreg,SIGNAL(triggered()),this,SLOT(reg_it()));
 }
+
+void MainWindow::on_copy()          //以下调用Qt中自带的拷贝、剪切、粘贴等函数
+{
+    ui->textEdit_2->copy();
+}
+void MainWindow::select_all()
+{
+    ui->textEdit_2->selectAll();
+}
+void MainWindow::cut_it()
+{
+    ui->textEdit_2->cut();
+}
+void MainWindow::paste_it()
+{
+    ui->textEdit_2->paste();
+}
+void MainWindow::redo_it()
+{
+    ui->textEdit_2->redo();
+}
+void MainWindow::reg_it()
+{
+    ui->textEdit_2->undo();
+}
+
 
 MainWindow::~MainWindow()
 {
--- bug_everyday_ide/mainwindow.h
+++ bug_everyday_ide/mainwindow.h
@@ -14,9 +14,15 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
 private:
     Ui::MainWindow *ui;
+ private slots:
+        void on_copy();       //声明复制函数
+        void select_all();     //声明全选函数函数
+        void paste_it();       //声明粘贴函数
+        void cut_it();           //声明剪切函数
+        void redo_it();         //声明重做函数
+        void reg_it();          //声明恢复函数
 };
 
 #endif // MAINWINDOW_H
